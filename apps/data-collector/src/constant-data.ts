4ee3278b973239d5e985a359ff4aa9f42b59490e
Farah <farah.fahhoum3@gmail.com>
2025-11-15 01:20:21 +0300

    Initial Commit

---
+++ apps/data-collector/src/constant-data.ts
@@ -0,0 +1,32 @@
+export const CITIES = [
+  {
+    name: "Riyadh",
+    latitude: 24.7136,
+    longitude: 46.6753,
+    countryCode: "SA",
+  },
+  {
+    name: "Dubai",
+    latitude: 25.2048,
+    longitude: 55.2708,
+    countryCode: "AE",
+  },
+  {
+    name: "Doha",
+    latitude: 25.2854,
+    longitude: 51.531,
+    countryCode: "QA",
+  },
+  {
+    name: "Muscat",
+    latitude: 23.588,
+    longitude: 58.3829,
+    countryCode: "OM",
+  },
+];
+
+export const CRITICAL_THRESHOLDS = {
+  PM2_5: 100, // µg/m³
+  PM10: 150, // µg/m³
+  UAQI: 100, // Universal AQI
+};
